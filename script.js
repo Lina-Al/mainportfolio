e2150098c3fecaabe06f4eab034c66ce0b538b1b
Lina-Al <98892615+Lina-Al@users.noreply.github.com>
2025-06-02 22:26:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,431 @@
+const data = [
+   {
+    module: "Курсовые",
+    subjects: [
+      {
+        name: "Курс 1",
+        tasks: [
+          { title: "Ссылка", link: "https://github.com/Lina-Al/Portfolio/blob/main/Курсовые/Курсовая-1-курс.pdf", pdf: "" }
+        ]},
+        {
+        name: "Курс 2",
+        tasks: [
+          { title: "Ссылка", link: "https://github.com/Lina-Al/Portfolio/blob/main/Курсовые/Курсовая-2-курс-Ал-Обайди-Лина-Моханадовна.pdf", pdf: "" }
+        ]},
+        {name: "Курс 3",
+        tasks: [
+          { title: "Ссылка", link: "https://github.com/Lina-Al/Portfolio/blob/main/Курсовые/Курсовая-3-курс-Ал-Обайди-Лина-Моханадовна.pdf", pdf: "" }
+        ]},
+        {name: "Курс4",
+        tasks: [
+          { title: "Ссылка", link: "https://github.com/Lina-Al/Portfolio/blob/main/Курсовые/Курсовая%204%20курс.pdf", pdf: "" }
+        ]
+      }
+      
+    ]
+  },
+     {
+    module: "Практика",
+    subjects: [
+      {
+        name: "Курс 1",
+        tasks: [
+          { title: "Ссылка на doc документы", link: "https://github.com/Lina-Al/-practiceLina1.github.io/blob/gh-pages/1.1.docx", pdf: "" }
+        ]},
+        {
+        name: "Курс 2",
+        tasks: [
+          { title: "Ссылка", link: "https://github.com/Lina-Al/Portfolio/tree/main/Практика/Практика%202%20курс", pdf: "" }
+        ]},
+        {name: "Курс 3",
+        tasks: [
+          { title: "Ссылка", link: "https://github.com/Lina-Al/Portfolio/tree/main/Практика/Практика%203%20курс", pdf: "" }
+        ]},
+        {name: "Курс 4",
+        tasks: [
+          { title: "Ссылка", link: "https://github.com/Lina-Al/Portfolio/tree/main/Практика/Практика%204%20курс", pdf: "" }
+        ]
+      }
+      
+    ]
+  },
+  {
+    module: "Компьютерная графика и управление информацией",
+    subjects: [
+        {
+        name: "Математические основы компьютерной графики",
+        tasks: [
+          { title: "лаборатрная 1", link: "https://colab.research.google.com/drive/1Y1SFqTec4QxVPMnnXlx91lm_Y5s-VYql", pdf: "" },
+          { title: "лаборатрная 2", link: "https://colab.research.google.com/drive/1LHK8_L8CyBM0B7YAghE30UaFK27s0lx9?usp=sharing", pdf: "" },
+          { title: "лаборатрная 3", link: "https://colab.research.google.com/drive/1LHK8_L8CyBM0B7YAghE30UaFK27s0lx9?usp=sharing", pdf: "" }
+        ]},
+
+
+        {name: "Компьютерная графика",
+        tasks: [
+          { title: "Лаборатоная 1", link: "https://disk.yandex.ru/d/-kxVFsQMsyxxRw", pdf: "" },
+          { title: "лаборатрная 2.Сертификат", link: "https://disk.yandex.ru/i/a9nZ3l76zAJALA", pdf: "" }
+        ]},
+        {name: "Инженерная графика",
+        tasks: [
+          { title: "Чертежи расположены по этой ссылке", link: "https://github.com/Lina-Al/Portfolio/tree/main/3Course/Инженерная%20графика", pdf: "" },                  
+        ]
+      }
+      
+    ]
+    
+  },
+  {
+    module: "Программирование",
+    subjects: [
+      {
+        name: "Программирование",
+        tasks: [
+          { title: "Семестр 6 лаборатрная 1", link: "https://replit.com/@LinaAlObaidi/Progh6-LR1-2-Al-Obaidi-Lina" },
+          { title: "Семестр 6 лаборатрная 3", link: "https://replit.com/@LinaAlObaidi/Progh6-LR1-2-Al-Obaidi-Lina" },
+          { title: "Семестр 6 лаборатрная 4", link: "https://github.com/Lina-Al/Portfolio/blob/main/3Course/6semProgramming/Копия_блокнота__Лабораторная_работа_4_5_ipynb_.ipynb", pdf: "" },
+          { title: "Семестр 6 Задание 5", link: "https://github.com/Lina-Al/Portfolio/blob/main/3Course/6semProgramming/5Labor.ipynb", pdf:""},
+          { title: "Семестр 6 лаборатрная 6 часть 1", link: "https://github.com/Lina-Al/Portfolio/blob/main/3Course/6semProgramming/ЛР6часть1.ipynb", pdf:"" },
+          { title: "Семестр 6 лаборатрная 6 часть 2", link: "https://github.com/Lina-Al/Portfolio/blob/main/3Course/6semProgramming/ЛР6часть2.ipynb", pdf: "" },
+          { title: "Семестр 6 лаборатрная 7", link: "https://github.com/Lina-Al/Portfolio/blob/main/3Course/6semProgramming/7Лаба.ipynb", pdf: "" },
+           { title: "Семестр 6 лаборатрная 8", link: "https://github.com/Lina-Al/Portfolio/blob/main/3Course/6semProgramming/8Лабораторная.md", pdf: "" },
+          { title: "Семестр 7 лаборатрная 3", link: "https://github.com/Lina-Al/PROG7lab3", pdf: "" },
+         { title: "Семестр 7 лаборатрная 4", link: "https://github.com/Lina-Al/PROG7Lab4", pdf: "" },
+          { title: "Семестр 7 лаборатрная 5", link: "https://github.com/Lina-Al/PROG7Lab6", pdf: "" },
+          { title: "Задания за 1-2 курс", link: "https://replit.com/repls/folder/programming", pdf: "" },
+          { title: "Остальные задания", link: "https://replit.com/repls/folder/сводка%20всех%20работ", pdf: "" }
+        ]
+      }
+    ]
+  },
+    {
+    module: "Организация и архитектура ЭВМ",
+    subjects: [
+      {
+        name: "Вычислительная техника",
+        tasks: [
+          { title: "Лаборатрная 1", link: "https://docs.google.com/document/d/1HNHoB-TubGHj2wMRpF53zHmNueiEFyh9GO90qUlOS0Q/edit?usp=drive_link", pdf: "" },
+          { title: "Лаборатрная 2", link: "https://docs.google.com/document/d/1cZY05QpLiEN15WV49u-Azlb57-mo7kjwY2c7HrKQj-s/edit?usp=drive_link", pdf: "" },
+          { title: "Лаборатрная 3", link: "https://docs.google.com/document/d/1K4GgTapJB7IPoh9HMHhgT5RX_GuQB9nuR66OP3DyfDg/edit?usp=drive_link", pdf: "" },
+          { title: "Лаборатрная 4", link: "https://docs.google.com/document/d/17PvR_IVUi31e4VjioG4GS5RjxACxPFqFDIk3xyi7xxA/edit?usp=drive_link", pdf: "" }
+        ]
+      },
+      {
+        name: "Операционные системы ",
+        tasks: [
+          { title: "Лабораторная 1", link: "https://colab.research.google.com/drive/1iC_LjKtZ0fWE-D46L4X22qxiV3k3bo5f#scrollTo=idFxh4WoMu4h", pdf: "" },
+
+          { title: "Лабораторная 2", link: "https://colab.research.google.com/drive/1Zfz0ERO8HflAVVvD0A3bAq1DtXSWc5_J", pdf: "" },
+          { title: "Лабораторная 3", link: "https://colab.research.google.com/drive/1W2SAmCmbzCmLg4xge-pGwaSyPTKkB5gg", pdf: "" }
+        ]
+      },{
+        name: "Сети и телекоммуникации",
+        tasks: [
+          { title: "лабораторная 1", link: "https://github.com/Lina-Al/Portfolio/blob/main/3Course/Сети%20и%20телекоммуникации/Ал-Обайди%20Лина%20Моханадовна%20работа1.pdf", pdf: "" },
+          { title: "лабораторная 2", link: "https://github.com/Lina-Al/Portfolio/blob/main/3Course/Сети%20и%20телекоммуникации/Ал-обайди%20Лина%20Моханадовна%20работа%202.pdf", pdf: "" },
+          { title: "лабораторная 3", link: "https://github.com/Lina-Al/Portfolio/blob/main/3Course/Сети%20и%20телекоммуникации/ЛР3%20Ал-Обайди%20Лина%20М..pdf", pdf: "" },
+          { title: "лабораторная 4", link: "https://github.com/Lina-Al/Portfolio/blob/main/3Course/Сети%20и%20телекоммуникации/ЛР%204%20Ал-Обайди%20Лина.pdf", pdf: "" },  
+          { title: "1с контррльная", link: "https://github.com/Lina-Al/Portfolio/blob/main/3Course/Сети%20и%20телекоммуникации/ЛР%204%20Ал-Обайди%20Лина.pdf", pdf: "" },  
+          { title: "1с конфигурация", link: "https://github.com/Lina-Al/Portfolio/blob/main/3Course/Сети%20и%20телекоммуникации/ЛР%204%20Ал-Обайди%20Лина.pdf", pdf: "" }    
+        ]
+      },
+      {
+        name: "Защита информации",
+        tasks: [
+          { title: "Ссылка на 1-3 задание", link: "https://disk.yandex.ru/d/LZyEhUyS4AI-DA", pdf: "" },
+          { title: "Лабораторная 4", link: "https://docs.google.com/document/d/1DsqL7FLhIQJt_TKM9ZI_9XQvK3a66i5EiASwXudL8N8/edit?usp=sharing", pdf: "" },
+          { title: "Лабораторная 5", link: "https://docs.google.com/document/d/1Qn6T90V1u5qTJOtHISFyH3Gl9izWf7nE_HcXCiKNnhg/edit?usp=sharing", pdf: "" },
+          { title: "Лабораторная 6", link: "https://docs.google.com/document/d/1QwuZ9Dtgw9u5ZQn4Ei5lzCI9liMVjqvB3Cj_YhBcBuk/edit?usp=sharing", pdf: "" },
+          { title: "Лабораторная 7", link: "https://docs.google.com/document/d/1RNJ4QsjJ7Q_xjo80khNQg7wPoBazobn0kM5PdZGmGGs/edit?usp=drive_link", pdf: "" }
+        ]
+      }
+    ]
+  }
+  ,
+    {
+    module: "Математика для инженеров",
+    subjects: [
+      {
+        name: "Линейная алгебра и теория матриц",
+        tasks: [
+          { title: "Папка с матрицами", link: "https://drive.google.com/drive/u/0/folders/1FxOPQT_w68-BPnHWTBTYZGitW9tmwpaa", pdf: "" },
+          { title: "Папка с СЛАУ", link: "https://drive.google.com/drive/folders/10069Fu7-YH4cnQaCZt-ts6bdpKma_djj?usp=drive_link", pdf: "" },
+          { title: "Папка с обратной матрицей", link: "https://drive.google.com/drive/folders/12D0y5xStAOWA3sAQM1TVgIfBSNBUkzDZ?usp=drive_link", pdf: "" },
+          { title: "Папка с обратной матрицей", link: "https://drive.google.com/drive/folders/12Y896lWBTpPydyoYqHTc6-B0f1FjFZmr?usp=drive_link", pdf: "" },
+          { title: "Папка с РАНГ", link: "https://drive.google.com/drive/folders/1z1yYLNkRLA1_QdYVNXIepUjV1CDUC3Od?usp=drive_link", pdf: "" },
+        ]
+      },
+      {
+        name: "Математический анализ",
+        tasks: [
+          { title: "Лабораторная 1", link: "https://replit.com/repls/folder/matanalis", pdf: "" }
+        ]
+      },
+      {
+        name: "Аналитическая геометрия",
+        tasks: [
+          { title: "Часть 1", link: "#", pdf: "" },
+          { title: "Часть 2", link: "#", pdf: ""},
+          { title: "Часть 2", link: "#", pdf: "" }
+        ]
+      },
+      {
+        name: "Интегралы и дифференциальные уравнения",
+        tasks: [
+          { title: "Ссылка на папку", link: "https://drive.google.com/drive/folders/1qPyBvQ1MgRocxf4zGPibLrezwIpdX1Y7?usp=drive_link", pdf: "" }
+        ]
+      }
+    ]
+  }
+  ,
+    {
+    module: "Информатика и физика для инженеров",
+    subjects: [
+      {
+        name: "Информатика",
+        tasks: [
+          { title: "Ссылка на папку", link: "https://github.com/Lina-Al/-practiceLina1.github.io/blob/gh-pages/1.1.docx", pdf: "" }
+        ]
+      }
+    ]
+  }
+  ,
+    {
+    module: "Технологии и методы вычислений",
+    subjects: [
+      {
+        name: "Анализ данных и основы Data science",
+        tasks: [
+          { title: "Собраны все задания ", link: "https://replit.com/repls/folder/AD", pdf: "" }
+        ]
+      },
+      {
+        name: "Технологии компьютерного моделирования",
+        tasks: [
+          { title: "Экзамен", link: "https://replit.com/@LinaAlObaidi/TKM-ekz", pdf: "" }
+        ]
+      }
+    ]
+  }
+  ,
+    {
+    module: "Информационные технологии",
+    subjects: [
+      {
+        name: "Техника и технологии представления и публикации информации",
+        tasks: [
+          { title: "Задание 1", link: "https://docs.google.com/document/d/19yha2qfwcBExrD2A5qNdVdBY_DknRLcdCeBkwc80DSc/edit?usp=sharing", pdf: "" }
+        ]
+      },
+      {
+        name: "Прикладные информационные технологии",
+        tasks: [
+          { title: "Срикнаст к 2 лабораторной", link: "https://drive.google.com/file/d/1vpL8YnQ6d9SRogTRYxUNoOL9lR1BRhCO/view?usp=sharing" , pdf: "" }
+        ]
+      },
+      {
+        name: "Основы корпоративного электронного обучения",
+        tasks: [
+          { title: "Лабораторная 1", link: "https://datalens.yandex/7e5r7kwwv4jqv", pdf: "" },
+           { title: "Лабораторная 2", link: "https://datalens.yandex/hoglidaydcca5", pdf: "" }
+        ]
+      },
+      {
+        name: "Информационные технологии и системы бизнес-аналитики",
+        tasks: [
+          { title: "Контрольная", link: "https://docs.google.com/document/d/1cKJcdYh7Aq8UyvYuoSv6QRBZn-hn68LmCuDsq78uw78/edit?usp=sharing", pdf: "" },
+          { title: "ЛР 1", link: "https://docs.google.com/document/d/1cKJcdYh7Aq8UyvYuoSv6QRBZn-hn68LmCuDsq78uw78/edit?usp=sharing", pdf: "" }
+
+        ]
+      },
+    ]
+  }
+  ,
+    {
+    module: "Информационные технологии в математике и физике",
+    subjects: [
+      {
+        name: "Информационные технологии в математике",
+        tasks: [
+          { title: "Папка с темой 1", link: "https://drive.google.com/drive/folders/1Aw0tqaZjQeUF-_boCz0vIE5IlEgslyrj?usp=drive_link", pdf: "" },
+          { title: "Папка с темой 2", link: "https://drive.google.com/drive/folders/1KcSSYFwoS7Kf9dY1m_P4bYNPHqMb2AsK?usp=drive_link", pdf: "" },
+          { title: "Папка с темой 3", link: "https://drive.google.com/drive/folders/1GiClRdTdcBo8MTvPBevhGG9tN2LLt_T6?usp=drive_link", pdf: "" },
+          { title: "Папка с темой 4", link: "https://drive.google.com/drive/folders/1TcaVDSXkXibxBcYhLKfiQF4rUUv9X2Ek?usp=drive_link", pdf: "" },
+          { title: "Папка с темой 5", link: "https://drive.google.com/drive/folders/1j4jR3il7-l5lNkL-THHMTVUk03Ge8-kO?usp=drive_link", pdf: "" },
+          { title: "Папка с темой 6", link: "https://drive.google.com/drive/folders/1nWYe7R3Q1T_NJsI_D6YMjWlMBg8VwCyj?usp=drive_link", pdf: "" }
+
+        ]
+      },
+      {
+        name: "Информационные технологии",
+        tasks: [
+          { title: "Папка с заданиями", link: "https://drive.google.com/drive/u/0/folders/1G6GojIZ5vhENr0cuX77vrJ9A0zkE1rll", pdf: "" }
+        ]
+      },
+      {
+        name: "Основы компьютерной алгебры",
+        tasks: [
+          { title: "Папка с заданиями", link: "https://drive.google.com/drive/u/0/folders/1z6E8df8XvI0O3KqZxHp1Lt0Wx0kM3WEi", pdf: "" }
+        ]
+      },
+    ]
+  }
+  ,
+    {
+    module: "Информационные ресурсы и средства профессиональной деятельности инженера",
+    subjects: [
+      {
+        name: "Мировые информационные ресурсы и цифровые библиотеки ",
+        tasks: [
+          { title: "Ссылка на папку", link: "https://disk.yandex.ru/d/NCPd2zvQFmC1iw", pdf: "" },
+        ]
+      },
+      {
+        name: "Социальные и профессиональные вопросы информатики и ИТ",
+        tasks: [
+          { title: "Ссылка на папку", link: "https://disk.yandex.ru/d/pxeeC80nWi6DUw", pdf: "" }
+        ]
+      },
+      {
+        name: "Математические основы глубокого обучения",
+        tasks: [
+           { title: "Лабораторная 2 ", link: "https://disk.yandex.ru/d/_5RnxPL3MvW_sA", pdf: ""}
+        ]
+      },
+      {
+        name: "Управление программными проектами",
+        tasks: [
+          { title: "Ссылка на папку", link: "https://disk.yandex.ru/d/kCjRuDRRGAeOGg", pdf: "" },
+         
+        ]
+      }
+    ]
+  }
+  ,
+    {
+    module: "Информационные технологии в управлении в IT-компании",
+    subjects: [
+      {
+        name: "Информационные технологии оценки персонала",
+        tasks: [
+          { title: "Ссылка на папку", link: "https://disk.yandex.ru/d/cXyTSB9v1BNF-Q", pdf: "" }
+        
+
+        ]
+      },
+      {
+        name: "IT-рекрутмент",
+        tasks: [
+          { title: "Ссылка на папку", link: "https://disk.yandex.ru/d/ZGOI-0G3EBiDxg", pdf: "" },
+        
+        ]
+      }
+    ]
+  }
+
+  ,
+    {
+       module: "Учебно-исследовательский",
+    subjects: [
+      {
+        name: "Учебно-технологический практикум",
+        tasks: [
+          { title: "Ссылка на Gitlab Gerzena", link: "https://git.herzen.spb.ru/212000/practice4course/tree/master", pdf: "" }
+        ]
+      },
+      {
+        name: "Научно-исследовательский проект",
+        tasks: [
+          { title: "Ссылка на папку", link: "https://disk.yandex.ru/d/ZGSpjTZZYNAFow", pdf: "" }
+        ]
+        },
+
+      {
+        name: "Языки написания спецификаций",
+        tasks: [
+          { title: "Диаграмма UML", link: "#https://disk.yandex.ru/i/6DPFoVxHFew64A", pdf: "" }
+        ]
+      },{
+        name: "Информационные средства и технологии инженерных и научных расчетов",
+        tasks: [
+          { title: "Ссылка на папку", link: "https://disk.yandex.ru/d/Dp4u52fEHH8pyA#", pdf: "" },
+       
+
+        ]
+      }
+    ]
+  },
+
+  {
+    module: "Информационные технологии и системы",
+    subjects: [
+      {
+        name: "Базы данных",
+        tasks: [
+          { title: "Ссылка на папку", link: "https://disk.yandex.ru/d/seWGWSNiBp5j6g", pdf: "" },
+        ]
+      },
+      {
+        name: "Инженерия знаний",
+        tasks: [
+          { title: "Ссылка на папку", link: "https://disk.yandex.ru/d/4wRWj7lfdzwcPg", pdf: "" }
+        ]
+      },
+    ]
+  }
+  
+
+];
+
+const container = document.getElementById("portfolio");
+
+data.forEach(mod => {
+  const modDiv = document.createElement("div");
+  modDiv.className = "block";
+
+  const modToggle = document.createElement("div");
+  modToggle.className = "toggle";
+  modToggle.textContent = mod.module;
+  const subjContainer = document.createElement("div");
+  subjContainer.className = "subject-container hidden";
+
+  modToggle.onclick = () => {
+    subjContainer.classList.toggle("hidden");
+  };
+
+  mod.subjects.forEach(subj => {
+    const subjDiv = document.createElement("div");
+
+    const subjTitle = document.createElement("div");
+    subjTitle.className = "toggle";
+    subjTitle.textContent = subj.name;
+    const taskContainer = document.createElement("div");
+    taskContainer.className = "task-container hidden";
+
+    subjTitle.onclick = () => {
+      taskContainer.classList.toggle("hidden");
+    };
+
+ subj.tasks.forEach(task => {
+  const taskDiv = document.createElement("div");
+  taskDiv.className = "task";
+
+  // Определяем, что использовать — link или pdf
+  const href = task.link && task.link !== "#" ? task.link : task.pdf;
+
+  taskDiv.innerHTML = `
+    <a href="${href}" target="_blank"><strong>${task.title}</strong></a>
+  `;
+
+  taskContainer.appendChild(taskDiv);
+});
+
+    subjDiv.appendChild(subjTitle);
+    subjDiv.appendChild(taskContainer);
+    subjContainer.appendChild(subjDiv);
+  });
+
+  modDiv.appendChild(modToggle);
+  modDiv.appendChild(subjContainer);
+  container.appendChild(modDiv);
+});
